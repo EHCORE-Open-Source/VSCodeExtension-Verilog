31d369e16a60f2611cc51bc1d373e9872f2fc18d
evaihchen <evaihchen@itri.org.tw>
2022-03-28 16:14:05 +0800

    modify syntax and sample

--- sample/sample.v
+++ sample/sample.v
@@ -1,73 +1,125 @@
 #define
-edge , negedge ,posedge edge123
-rcmos (rpmos ,rnmos
-cmos nmos pmos
-supply0 supply1 supply01
-tri tri0 tri1 tri01 
-triand trior trireg
-wand wor wire
-scalared vectored scalar vector
-inout ,inout (inout
-input output
-   always@
-always 
-initial
-integer real realtime time reg
-highz0 ,highz1 highz01 
-(pull0 pull1
-strong0 strong1
-weak0 weak1 weak
-large medium small
-parameter
-specparam
-strong0 strong1
-event
-parameter
-defparam
-specparam
+edge (negedge clk)
+(posedge clk, negedge rst)
 
+tri[15:0] 
+tri0(
+tri1#300 
+triand 
+trior
+wand
+wor 
+wire
+
+rcmos( , , )
+rpmos (
+rnmos
+cmos 
+nmos 
+pmos
+trireg(small)
+
+(supply1,supply0) 
+( supply0, supply1 )
+supply0#300 supply1[30:0] supply0 ()
+
+(highz0,highz1) 
+( pull0, pull1 )
+(strong0) ( strong1 ) 
+weak0 weak1
+
+(large) (medium) ( small )
+
+(large)scalared[63:0] vectored [31:0]
+
+(input a, b ,input c, output d, e) 
+output[3:0] inout 
+
+   always@(  
+always 
+   initial@
+initial
+
+function integer a;
+real r1=2.5 
+realtime rtime, 
+time t1 = 25 
+
+reg[30:0] 
+(output reg[3:0] hund, tens, ones)
+
+event do_dump;
+parameter[3:0]
+parameter r=5.7
+defparam f1=3.14
+specparam t0 =12
+specparam[30:0]
 
 #action
-assign
-deassign
-force
-release
-begin begin :begin )begin
-case
-casex
-casez
-function
-module
-macromodule
-primitive
-specify
-table
-task
-end
-endcase
+assign{i1,i2,i3,i4,i5,i6,i7,i8,i9,i10}
+assign(strong1, pull0)
+assign#300
+
+assign q=0
+deassign q
+force d=(a|b|c)
+release d
+
+always @*begin 
+begin: countls
+while (condition)begin
+
+case (WIDTH)
+   1:begin: adder
+   2:begin: adder
+
+function[7:0]
 endfunction
+function integer clogb2;
+
+module
 endmodule
+macromodule
+endmodule
+primitive
 endprimitive
+specify
 endspecify
+table
 endtable
+task
 endtask
 fork
 join
-default
-disable
-for
-forever
-else
-ifnone
-repeat
-wait
-while
-if if(
-and ,and (nand
-buf not
-bufif0 bufif1 bufif01
+#100 fork:innerwave
+end
+
+default:
+default 
+
+case()
+endcase
+casex()
+casez
+if()  
+ifnone() for while wait(!enable) 
+
+a =repeat(3)
+repeat(500)
+
+disable block_name
+@reset disable event_expr
+
+else;
+else if
+
+forever@(negedge clock)
+forever#1000
+
+and(small) nand#5 xor nor xnor or
+always@(posedge clock or posedge reset or )
+buf(q, ffout) buf#10000 not
+bufif0 bufif1
 notif0 notif1
-pulldown pullup
-tran tranif0 tranif1
-rtran rtranif0 rtranif1
-xor nor xnor or
+pulldown(strength0) pullup(strength1)
+tran tranif0 tranif1 rtran rtranif0 rtranif1
